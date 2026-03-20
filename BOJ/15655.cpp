2874596227a7efae6d123845c71539a6a237abf0
hhz <itree1030@gmail.com>
2026-03-20 15:14:25 +0900

    BOJ - 15655 N과 M(6)

---
+++ BOJ/15655.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+
+int arr[10];
+int nums[10];
+bool isUsed[10];
+
+
+void func(int curr, int mn)
+{
+	if (curr == M) {
+		for (int i = 0; i < M; ++i) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < N; ++i) {
+		if (!isUsed[i] && nums[i] > mn) {
+			isUsed[i] = true;
+			arr[curr] = nums[i];
+			func(curr + 1, nums[i]);
+			isUsed[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin >> N >> M;
+
+	for (int i = 0; i < N; ++i) {
+		int a;
+		cin >> a;
+		nums[i] = a;
+	}
+
+	sort(nums, nums + N);
+
+	func(0, 0);
+}
