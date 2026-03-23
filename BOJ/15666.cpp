ec5121efc7e4acf75937c4fc23a3b8911cdf6e67
hhz <itree1030@gmail.com>
2026-03-23 09:45:59 +0900

    BOJ - 15666 N과 M(12)

---
+++ BOJ/15666.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+
+int arr[9];
+int nums[9];
+
+void func(int curr, int mn)
+{
+	if (curr == M) {
+		for (int i = 0; i < M; ++i) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < N; ++i) {
+		if (mn <= nums[i]) {
+			arr[curr] = nums[i];
+			func(curr + 1, nums[i]);
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; ++i) {
+		int a;
+		cin >> a;
+		nums[i] = a;
+	}
+
+	sort(nums, nums + N);
+
+	N = unique(nums, nums + N) - nums;
+
+	func(0, 0);
+}
